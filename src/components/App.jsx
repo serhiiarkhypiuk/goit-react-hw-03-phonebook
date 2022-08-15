ed6ac4492b27a835810e70c942c5aa4e6c0d82fe
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-08-15 09:43:05 +0300

    demo contacts added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,61 @@
-import React, { Component } from "react"
-import ContactForm from "./organisms/Form/Form"
-import Title from "./atoms/Title/Title"
-import Filter from "./atoms/Input/Input"
-import ContactsList from "./atoms/ContactsList/ContactsList"
-import styled from "styled-components"
+import React, { Component } from 'react';
+import ContactForm from './organisms/Form/Form';
+import Title from './atoms/Title/Title';
+import Filter from './atoms/Input/Input';
+import ContactsList from './atoms/ContactsList/ContactsList';
+import styled from 'styled-components';
 
 class App extends Component {
   state = {
-    contacts: [],
-    filter: ""
-  }
-  
-  onSubmit = ( data ) => {
-    if (this.state.contacts.find((contact) => contact.name === data.name)) {
-      alert(`${data.name} is already in contacts`)
-      return
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  onSubmit = data => {
+    if (this.state.contacts.find(contact => contact.name === data.name)) {
+      alert(`${data.name} is already in contacts`);
+      return;
     }
 
     this.setState(({ contacts }) => ({
-      contacts: [data, ...contacts]
-    }))
-  }
+      contacts: [data, ...contacts],
+    }));
+  };
 
-  onSearch = ( event ) => {
-    const searchQuery = event.target.value
+  onSearch = event => {
+    const searchQuery = event.target.value;
     this.setState({
-      filter: searchQuery
-    })
-  }
-  
-  deleteContact = ( contactId ) =>
+      filter: searchQuery,
+    });
+  };
+
+  deleteContact = contactId =>
     this.setState({
-      contacts: this.state.contacts.filter((contact) => contact.id !== contactId),
-  })
-  
-  componentDidUpdate( prevState ) {
-    if ( this.state.contacts !== prevState.contacts ) localStorage.setItem('Contacts', JSON.stringify(this.state.contacts))
+      contacts: this.state.contacts.filter(contact => contact.id !== contactId),
+    });
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts)
+      localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
   }
 
   componentDidMount() {
-    const contacts = localStorage.getItem('Contacts')
-    const parsedContacts = JSON.parse(contacts)
+    const contacts = localStorage.getItem('Contacts');
+    const parsedContacts = JSON.parse(contacts);
 
-    if ( parsedContacts ) this.setState({ contacts: parsedContacts })
+    if (parsedContacts) this.setState({ contacts: parsedContacts });
   }
 
   render() {
-    const normalizedFilter = this.state.filter.toLowerCase()
-    const displayedContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const displayedContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
 
     return (
       <StyledWrapper>
@@ -61,12 +69,17 @@ class App extends Component {
           value={this.state.filter}
           onChange={this.onSearch}
           required={false}
-        /> {displayedContacts.length === 0 && this.state.filter.length > 0
-          ? (<p>No results for your search</p>)
-          : <ContactsList displayedContacts={displayedContacts} onClick={this.deleteContact} />
-          }
+        />{' '}
+        {displayedContacts.length === 0 && this.state.filter.length > 0 ? (
+          <p>No results for your search</p>
+        ) : (
+          <ContactsList
+            displayedContacts={displayedContacts}
+            onClick={this.deleteContact}
+          />
+        )}
       </StyledWrapper>
-    )
+    );
   }
 }
 
@@ -81,6 +94,6 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`
+`;
 
-export default App
+export default App;
